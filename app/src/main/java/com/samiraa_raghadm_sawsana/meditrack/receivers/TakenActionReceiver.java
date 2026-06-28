6a6be15fc082fe6a133c626d445614a322ce8afa
raghad-mu <muhissenrahgad@gmail.com>
2026-06-28 19:44:28 +0300

    feat: integrate NotificationHelper in TakenActionReceiver for improved notification management

--- app/src/main/java/com/samiraa_raghadm_sawsana/meditrack/receivers/TakenActionReceiver.java
+++ app/src/main/java/com/samiraa_raghadm_sawsana/meditrack/receivers/TakenActionReceiver.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.samiraa_raghadm_sawsana.meditrack.helpers.AppExecutors;
+import com.samiraa_raghadm_sawsana.meditrack.helpers.NotificationHelper;
 import com.samiraa_raghadm_sawsana.meditrack.helpers.PrefsManager;
 import com.samiraa_raghadm_sawsana.meditrack.models.IntakeLog;
 import com.samiraa_raghadm_sawsana.meditrack.models.Medication;
